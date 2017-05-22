a909e62951c43043e9164969cb76b29b9d666db8
eliagbayani <eli@eol.org>
2017-05-21 21:23:07 -0400

    quickfix

--- templates/freshdata/monitors-form.php
+++ templates/freshdata/monitors-form.php
@@ -29,7 +29,7 @@ Array
         <table>
             <tr><td>uuid:</td>           <td id="value"><?php echo $monitor['selector']['uuid'] ?></td></tr>
             <tr><td>Taxa:</td>           <td id="value"><?php echo $monitor['selector']['taxonSelector'] ?></td></tr>
-            <tr><td>Status:</td>         <td id="value"><?php echo $monitor['status'] ?></td></tr>
+            <tr><td>Status:</td>         <td id="value"><?php echo @$monitor['status'] ?></td></tr>
             <tr><td>Records:</td>        <td id="value"><?php echo number_format($monitor['recordCount']) ?></td></tr>
             <tr><td>Trait selector:</td> <td id="value"><?php echo $monitor['selector']['traitSelector'] ?></td></tr>
             <tr><td>String:</td>         <td id="value"><?php echo $monitor['selector']['wktString'] ?></td></tr>
