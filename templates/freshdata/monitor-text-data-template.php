6a1d2183a90fda9e7f495d1b2cd08fc220d0adc1
eliagbayani <eli@eol.org>
2018-01-22 02:05:18 -0500

    mtce

--- templates/freshdata/monitor-text-data-template.php
+++ templates/freshdata/monitor-text-data-template.php
@@ -2,7 +2,12 @@
     <tr><td colspan="2"><hr><b>Archive Info:</b><hr></td></tr>
     <?php
     $fields = array("uuid_archive", "Taxa", "Status", "Records", "Trait_selector", "String", "tsv_url");
-    foreach($fields as $field) echo "<tr><td>$field:</td><td id='value'>".@$rec_from_text[$field]."</td></tr>";
+    foreach($fields as $field) 
+    {
+        $tfield = $field;
+        if($tfield == "tsv_url") $tfield = "TSV URL";
+        echo "<tr><td>$tfield:</td><td id='value'>".@$rec_from_text[$field]."</td></tr>";
+    }
     
     if(!isset($search_url)) $search_url = self::generate_freshdata_search_url($rec_from_text);
     if($search_url)
