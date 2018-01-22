2f15968fcab9063a76249beed74800fea49abe9c
eliagbayani <eli@eol.org>
2018-01-22 06:02:27 -0500

    cleaning

--- templates/freshdata/monitor-save-add.php
+++ templates/freshdata/monitor-save-add.php
@@ -14,7 +14,6 @@ if($ctrler->save_to_text($params))
 {
     $ctrler->save_manually_added_uuid($params['uuid_archive']);
     
-    
     // echo "<br><span id='memo'>Saved OK</span><br>";
     $ctrler->display_message(array('type' => "highlight", 'msg' => "Record has been successfully created. Click <b>Proceed</b>."));
     ?>
@@ -34,14 +33,12 @@ if($ctrler->save_to_text($params))
         echo "<tr><td>$field:</td><td id='value'>".$params[$field]."</td></tr>";
     }
     ?>
-
     <tr><td colspan="2"><hr><b>Additional Fields:</b><hr></td></tr>
     <tr><td>Title:</td>        <td id="value"><?php echo $params['Title']       ?></td></tr>
     <tr><td>Description:</td>  <td id="value"><?php echo $params['Description'] ?></td></tr>
     <tr><td>URL:</td>          <td id="value"><?php echo $params['URL']         ?></td></tr>
     <tr><td>Training materials:</td> <td id="value"><?php echo $params['Training_materials'] ?></td></tr>
     <tr><td>Contact:</td>            <td id="value"><?php echo $params['Contact']            ?></td></tr>
-
     </table>
     <?php
 }
--- templates/freshdata/monitor-save.php
+++ templates/freshdata/monitor-save.php
@@ -31,14 +31,12 @@ if($ctrler->save_to_text($params))
         echo "<tr><td>$field:</td><td id='value'>".$params[$field]."</td></tr>";
     }
     ?>
-
     <tr><td colspan="2"><hr><b>Additional Fields:</b><hr></td></tr>
     <tr><td>Title:</td>        <td id="value"><?php echo $params['Title']       ?></td></tr>
     <tr><td>Description:</td>  <td id="value"><?php echo $params['Description'] ?></td></tr>
     <tr><td>URL:</td>          <td id="value"><?php echo $params['URL']         ?></td></tr>
     <tr><td>Training materials:</td> <td id="value"><?php echo $params['Training_materials'] ?></td></tr>
     <tr><td>Contact:</td>            <td id="value"><?php echo $params['Contact']            ?></td></tr>
-
     </table>
     <?php
 }
