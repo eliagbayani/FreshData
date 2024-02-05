db097446c3310c6a74854929eb73ca7fb505142b
eliagbayani <eagbayani@eol.org>
2024-02-05 21:37:02 +0800

    /html replaced

--- controllers/other.php
+++ controllers/other.php
@@ -236,7 +236,10 @@ class other_controller
     function generate_tsv_filepath($basename, $useIn = "host")
     {
         $host    = __DIR__ . "/../TSV_files/".$basename.".tsv";
-        $jenkins = "/html/FreshData/TSV_files/".$basename.".tsv";
+        
+        // $jenkins = "/html/FreshData/TSV_files/".$basename.".tsv";
+        $jenkins = "/var/www/html/FreshData/TSV_files/".$basename.".tsv"; //2024
+
         if($useIn == "host") return $host;
         elseif($useIn == "jenkins") {
             if(PHP_PATH == '/opt/homebrew/opt/php@5.6/bin/php') return $host;
@@ -271,7 +274,10 @@ class other_controller
     function generate_sh_filepath($basename)
     {
         if(PHP_PATH == '/opt/homebrew/opt/php@5.6/bin/php') return __DIR__ . "/../sh_files/".$basename.".sh";
-        elseif(PHP_PATH == 'php')            return "/html/FreshData/sh_files/".$basename.".sh";
+        elseif(PHP_PATH == 'php') {
+            // return "/html/FreshData/sh_files/".$basename.".sh";
+            return "/var/www/html/FreshData/sh_files/".$basename.".sh"; //2024
+        }
     }
     function build_curl_cmd_for_jenkins($cmd, $jenkins_job, $cmd2 = null) //for download of TSV files
     {
